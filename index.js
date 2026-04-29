dd2e273f376b1c643088da60c54eea931a345173
Breanna47 <breanna.watson@yahoo.com>
2026-04-29 09:42:48 -0400

    pt

--- index.js
+++ index.js
@@ -1,36 +1,33 @@
-//template_j6j387c
-//service_2ctgnmr
-//YBRl1eTUb3QtZzYrq
 
 function contact(event) {
     event.preventDefault();
     const loading = document.querySelector('.modal__overlay--loading');
 const success = document.querySelector('.modal__overlay--success');
-loading.classlist += " modal__overlay--visible";
-   emailjs
+loading.cLassList += " modal__overlay--visible";
+   emailjs.
     sendForm(
     'service_2ctgnmr', 
-'template_j6j387c',
+'template_kwdb8wo',
 event.target,
 'YBRl1eTUb3QtZzYrq'
   ) 
 .then(() => {
-   loading.classlist.remove("modal__overlay--visible");
-   success.classlist += " modal__overlay--visible";
+   loading.classList.remove("modal__overlay--visible");
+   success.classList += " modal__overlay--visible";
 }).catch(() => {
-    loading.classlist.remove("modal__overlay--visible");
+    loading.classList.remove("modal__overlay--visible");
     alert(
         "The email service is temporarily unabilable. Please contact me"
     );
 })
 }
-
 let isModalOpen = false;
-function toggleModal() {
-    isModalOpen = !isModalOpen;
-    if (isModalOpen) {
-        return document.body.classlist.remove("modal--open");
-    }
-    document.body.classlist += " modal-open";
 
+function toggleModal() {
+  if (isModalOpen) {
+    isModalOpen = false;
+    return document.body.classList.remove("modal__open");
+  }
+  isModalOpen = true;
+  document.body.classList += " modal__open";
 }
