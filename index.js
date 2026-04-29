b7b8b0f06bfb840acfbff644524593d4104530d1
Breanna47 <breanna.watson@yahoo.com>
2026-04-29 17:27:21 -0400

    exit not reactive

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 function contact(event) {
     event.preventDefault();
     const loading = document.querySelector('.modal__overlay--loading');
-const success = document.querySelector('.modal__overlay--success');
+const success = document.querySelector(' .modal__overlay--success');
 loading.cLassList += " modal__overlay--visible";
    emailjs.
     sendForm(
