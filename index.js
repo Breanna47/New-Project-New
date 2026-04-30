d63cd3837f95d0aeaeca8cbcf084356c2704bc51
Breanna47 <breanna.watson@yahoo.com>
2026-04-29 23:39:06 -0400

    completed

--- index.js
+++ index.js
@@ -1,4 +1,30 @@
+let isModalOpen = false;
+let contrastToggle = false;
+const scaleFactor = 1 / 20;
 
+function moveBackground(event) {
+  const shapes = document.querySelectorAll(".shape");
+  const x = event.clientX * scaleFactor;
+  const y = event.clientY * scaleFactor;
+  console.log(x, y)
+
+for (let i = 0; i < shapes.length; ++i) {
+  const isOdd = i % 2 !== 0;
+  const oddInteger = isOdd ? -1 : 1;
+shapes[i].style.transform = `translate(${x * oddInteger}px, ${y * oddInteger}px)`
+}
+}
+
+function toggleContrast() {
+  contrastToggle = !contrastToggle
+  if (contrastToggle) {
+  document.body.classList += " dark-theme"
+  }
+  else {
+      document.body.classList.remove("dark-theme")
+
+  }
+}
 function contact(event) {
     event.preventDefault();
     const loading = document.querySelector('.modal__overlay--loading');
@@ -21,7 +47,6 @@ event.target,
     );
 })
 }
-let isModalOpen = false;
 
 function toggleModal() {
   if (isModalOpen) {
